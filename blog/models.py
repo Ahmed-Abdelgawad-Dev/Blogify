99c5fac9360d8dd16aed439fc72a0d56d312a0b3
ahmed-abdelgawad-dev <ahmed.abdelgawad.dev@gmail.com>
2022-10-07 02:58:27 +0400

    Slugified urls with dates with unique_to_date, pagination funtionality in post view

--- blog/models.py
+++ blog/models.py
@@ -1,38 +1,48 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
 def get_inf_time():
-	return timezone.now()
+    return timezone.now()
 
 
 class PublishedManager(models.Manager):
-	"""Published Posts' Manager"""
+    """Published Posts' Manager"""
 
-	def get_queryset(self):
-		return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
-	class Status(models.TextChoices):
-		DRAFT = 'DF', 'Draft'
-		PUBLISHED = 'PB', 'Published'
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
 
-	title = models.CharField(max_length=255)
-	slug = models.SlugField(max_length=255)
-	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-	body = models.TextField()
-	publish = models.DateTimeField(default=get_inf_time)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
-	status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
-	objects = models.Manager()  # Default Manager
-	published = PublishedManager()  # CustomModel Manager
+    title = models.CharField(max_length=255)
+    # Unique for the date ONLY not for the time.
+    slug   = models.SlugField(max_length=255, unique_for_date='publish')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    body    = models.TextField()
+    publish = models.DateTimeField(default=get_inf_time)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status  = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT)
+    objects   = models.Manager()  # Default Manager
+    published = PublishedManager()  # CustomModel Manager
 
-	class Meta:
-		ordering = ['-publish']
-		indexes = [models.Index(fields=['-publish']), ]
+    class Meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish']), ]
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[
+            self.publish.year, self.publish.month,
+            self.publish.day, self.slug
+        ])
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 
 
 def post_list(request):
-	posts = Post.objects.all()
-	return render(request, 'blog/post/list.html', {'posts': posts})
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list,6)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
-	post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-	return render(request, 'blog/post/detail.html', {'post': post})
+def post_detail(request, year: int, month: int, day: int, post: str):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
