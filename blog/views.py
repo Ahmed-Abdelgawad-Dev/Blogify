aab6a63576eadeca264aacb3b25988e13956484e
ahmed-abdelgawad-dev <ahmed.abdelgawad.dev@gmail.com>
2022-10-08 19:24:33 +0400

    post_share view + form to send an email to recommend a post implemented

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,32 @@
+import os
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
+from .forms import EmailPostForm
 
-#CBV
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent =False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            clean_data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{clean_data['name']} recommends you to readThis:  " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{clean_data['name']}\'s comments: {clean_data['comments']}"
+            send_mail(subject, message, os.environ.get('DEFAULT_FROM_EMAIL'), [clean_data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+# CBV
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
@@ -11,18 +34,18 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-# FBV
-def post_list(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list,3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# # FBV
+# def post_list(request):
+#     post_list = Post.objects.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year: int, month: int, day: int, post: str):
--- blogify/settings.py
+++ blogify/settings.py
@@ -83,6 +83,16 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Email server configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -94,6 +104,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+
+
 
 
 
